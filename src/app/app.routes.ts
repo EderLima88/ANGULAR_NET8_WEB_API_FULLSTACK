f079f8702a5f62499fee914f30ee6ddc23802fa6
Eder de Lima <eder.ebl@gmail.com>
2024-03-07 14:01:22 -0300

    Commit unico

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { StudentsComponent } from './students/students.component';
+import { StudentFormComponent } from './students/student-form/student-form.component';
 
-export const routes: Routes = [];
+//aqui os caminhos do routerlink usado nos componentes html
+export const routes: Routes = [
+    {
+        path: 'students',
+        component: StudentsComponent
+    },
+    {
+        path: 'student/form',
+        component: StudentFormComponent
+    },
+    {
+        path: 'students/:id',
+        component: StudentFormComponent
+    }
+   
+];
